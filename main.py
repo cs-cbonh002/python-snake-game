c07f01632aba08245f3edbb90c527a533174d92d
cs-cbonh002 <bonham_36@hotmail.com>
2024-03-29 18:07:57 -0400

    Add snake starting pixel and food (target) to game window.

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 # Example file showing a basic pygame "game loop"
 import pygame
+import random
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 running = True
+
+
+def generate_starting_position():
+    position_range = (pixel_width // 2, screen_width - pixel_width // 2, pixel_width)
+    return [random.randrange(*position_range), random.randrange(*position_range)]
+
+
+# playground
+pixel_width = 50
+
+# snake
+snake_pixel = pygame.rect.Rect([0, 0, pixel_width - 2, pixel_width - 2])
+snake_pixel.center = generate_starting_position()
+snake = [snake_pixel.copy()]
+
+# food (target)
+target = pygame.rect.Rect([0, 0, pixel_width - 2, pixel_width - 2])
+target.center = generate_starting_position()
 
 while running:
     # poll for events
@@ -15,9 +36,13 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
 
     # RENDER YOUR GAME HERE
+    for snake_part in snake:
+        pygame.draw.rect(screen, "green", snake_part)
+
+    pygame.draw.rect(screen, "red", target)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
